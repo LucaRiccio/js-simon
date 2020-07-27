80d2b5314b1503f70ecb695ff3c4eaeeb526a450
LucaRiccio <lucariccio888@gmail.com>
2020-07-27 15:19:39 +0200

    Alert 5 numeri random + prompt dopo tot secondi

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Un alert espone 5 numeri casuali.
+// Dopo la chiusura (manuale, cioè facendo click su ok) dell'alert, parte un timer di 30 secondi.
+// Dopo i 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software dice quanti e
+// quali dei numeri da indovinare sono stati individuati.
+
+var numeriCasuali = [];
+var numeriUtente = [];
+
+for (var i = 0; i < 5; i++) {
+  numeriCasuali.push(generaRandom(1,100));
+}
+console.log(numeriCasuali);
+
+alert("Numeri da memorizzare: " + numeriCasuali);
+
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+  alert('Hello');
+};
+
+
+
+
+
+
+
+
+
+// ***FUNZIONI***
+
+function generaRandom(min,max) {
+  return Math.floor(Math.random()*(max - min + 1) + min);
+}
